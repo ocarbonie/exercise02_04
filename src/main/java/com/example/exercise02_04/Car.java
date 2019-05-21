b7e5d035497a736a7dbc87bc220bfa17897f8b58
Oona Carbonie <carbonie.oona@gmail.com>
2019-05-21 16:24:28 -0400

    Initial commit

---
+++ src/main/java/com/example/exercise02_04/Car.java
@@ -0,0 +1,43 @@
+package com.example.exercise02_04;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Car {
+    @NotNull
+    @Size(min = 1, max = 20)
+    private String name;
+
+    @NotNull
+    @Size(min = 1, max = 20)
+    private String type;
+
+    @NotNull
+    @Size(min = 1, max = 20)
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ src/main/java/com/example/exercise02_04/Exercise0204Application.java
@@ -0,0 +1,13 @@
+package com.example.exercise02_04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise0204Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise0204Application.class, args);
+	}
+
+}
